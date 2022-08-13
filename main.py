f40b0e7521f4e70a4853017081f4c0046849fe89
209 <209@academicos.cesde.edu.co>
2022-08-13 13:00:17 -0500

    se crea proyecto python

---
+++ main.py
@@ -0,0 +1,39 @@
+#Hola mundo con Python
+
+'''' -comentario de bloque '''
+
+#SALIDA POR CONSOLA
+print ("Hola mundo soy Juan Jose")
+print ('Hola mundo soy Juan Jose')
+
+
+#Entradas
+#Datos primitivos
+dato=None
+nombre= "Angie Varela"
+edad=23
+estatura= 1.63
+hinchaDelMejor = True  #los buleanos van con T o F mayuscula 
+
+
+#Salida por teclado
+print("Hola ",nombre, "tu edad es: ",edad)
+print(f'Hola {nombre} tu edad es {edad}')
+
+
+#Recibir datos por teclado
+ciudad=input("Digita la ciudad donde vives: ")
+print(f'La ciudad donde usted vive es: {ciudad}')
+
+
+#Recibir datos numericos por teclado
+numero1= int(input("Digita un número: "))
+numero2= int(input("Digita otro número "))
+print(f'El numero uno es {numero1}' )
+print(f'El numero dos es {numero2}' )
+
+
+#Operadores aritmeticos
+#(+ - * / %)
+suma=numero1+numero2
+print(f'la suma es {suma}')
---
+++ reto.py
@@ -0,0 +1,14 @@
+
+#EJERCICIO
+
+numero=int(input("Digite un múmero entero "))
+
+resultado=numero%3
+print(f'El residuo es: {resultado}')
+
+#Condicional simple en Python
+if(resultado==0):
+    print("Es multiplo de 3")
+else:
+    print("no es multiplo de 3")
+print("fin del programa")
